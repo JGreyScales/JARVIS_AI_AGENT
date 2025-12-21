f27bdfcc22a28551b08233c4785b7df748942652
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-12-21 15:34:34 -0500

    hand landmarks from still frame

--- BACKEND/ImageProcessor/handLandmarks.py
+++ BACKEND/ImageProcessor/handLandmarks.py
@@ -1,4 +1,23 @@
-import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+
+import os
+
+model_path = os.path.join(os.path.dirname(__file__), 'models', 'hand_landmarker.task')
+print("Model path:", model_path)
+baseOptions = mp.tasks.BaseOptions
+handLandMarker = mp.tasks.vision.HandLandmarker
+handLandMarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+visionRunningMode = mp.tasks.vision.RunningMode
+
+options = handLandMarkerOptions(base_options=baseOptions(model_asset_path=model_path),
+                                running_mode=visionRunningMode.IMAGE)
+
+
+with handLandMarker.create_from_options(options) as landmarker:
+    hand_path = os.path.join(os.path.dirname(__file__), 'hand.jpg')
+    print("Hand path:", hand_path)
+    mpImage = mp.Image.create_from_file(hand_path)
+    handLandmarkerResult = landmarker.detect(mpImage)
+    print(handLandmarkerResult.hand_world_landmarks  )
