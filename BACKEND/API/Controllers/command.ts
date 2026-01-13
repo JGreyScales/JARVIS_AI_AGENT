5b75a3741428c12210ddd335d43dc6fda694a21b
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2026-01-13 04:26:49 -0500

    Update command.ts

--- BACKEND/API/Controllers/command.ts
+++ BACKEND/API/Controllers/command.ts
@@ -1,32 +1,41 @@
-import familyObject from "../DataAccessors/Families";
-import UserObject from "../DataAccessors/User";
 import commandsObject from "../DataAccessors/commands";
 
 
 class command {
-    commands: commandsObject[];
-    user: UserObject;
-    family: familyObject;
+    commandObj: commandsObject;
 
-    constructor(user: UserObject, family: familyObject) {
-        this.commands = this.getCommands();
-        this.user = user;
-        this.family = family;
+    constructor(commandObj: commandsObject) {
+        this.commandObj = commandObj;
     }
 
-    private getCommands(): commandsObject[] {
-        return []
-    }
-
-    getCommand(queryCommandName: string): commandsObject | undefined {
-        const result: commandsObject | undefined = this.commands.find(({ commandName }) => commandName === queryCommandName);
-        if (result === undefined){
-            console.log("Command is undefined");
-        }
-        return result;
-    }
-
-    executeCommand(index: number){
-
+    executeCommand(){
+        console.log(this.commandObj.commandExecution)
     }
 }
+
+
+// this would be better in the family class
+
+// class command {
+//     commands: commandsObject[];
+//     user: UserObject;
+//     family: familyObject;
+
+//     constructor(user: UserObject, family: familyObject) {
+//         this.commands = this.getCommands();
+//         this.user = user;
+//         this.family = family;
+//     }
+
+//     private getCommands(): commandsObject[] {
+//         return []
+//     }
+
+//     getCommand(queryCommandName: string): commandsObject | undefined {
+//         const result: commandsObject | undefined = this.commands.find(({ commandName }) => commandName === queryCommandName);
+//         if (result === undefined){
+//             console.log("Command is undefined");
+//         }
+//         return result;
+//     }
+// }
